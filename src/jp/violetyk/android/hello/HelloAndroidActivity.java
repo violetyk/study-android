31c93cc80e313af64cf5aa24a14fe7b58ca6f79f
violetyk <yuhei.kagaya@gmail.com>
2012-07-02 14:32:04 +0900

    Add menu.

--- src/jp/violetyk/android/hello/HelloAndroidActivity.java
+++ src/jp/violetyk/android/hello/HelloAndroidActivity.java
@@ -2,7 +2,13 @@ package jp.violetyk.android.hello;
 
 
 import android.app.ListActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +20,8 @@ import android.widget.Toast;
 // ListActivityクラスを継承してOnItemClickListenerを実装
 // public class HelloAndroidActivity extends Activity {
 public class HelloAndroidActivity extends ListActivity implements OnItemClickListener {
+	
+	private final String TAG = "HELLOANDROID";
 	
     /** Called when the activity is first created. */
     @Override
@@ -47,5 +55,53 @@ public class HelloAndroidActivity extends ListActivity implements OnItemClickLis
 		TextView item = (TextView) v;
 		Toast.makeText(this, item.getText(), Toast.LENGTH_SHORT).show();
 		
+	}
+	
+    // 端末の[menu]ボタンを押されたときに呼び出されるメソッド。
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.global, menu);
+		return true;
+	}	
+	
+	
+	// オプションメニューのアイテムが選択されたときに呼び出されるメソッド。
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch(item.getItemId()) {
+			case R.id.menu1:
+				Toast.makeText(this, "menu1", Toast.LENGTH_SHORT).show();
+				return true;
+				
+			case R.id.menu2:
+				// メーラー起動インテントの作成
+				Intent mailIntent = new Intent(Intent.ACTION_SEND);
+				mailIntent.putExtra(Intent.EXTRA_SUBJECT, "テストの件");
+				mailIntent.putExtra(Intent.EXTRA_TEXT, "テストです。");
+				mailIntent.setType("message/rfc822");
+				
+				
+				try {
+					// システムにインストールされているメーラーを立ち上げる。
+					
+					startActivity(mailIntent);
+					
+				} catch (ActivityNotFoundException e) {
+					// インテントの設定ミス？
+					Log.e(TAG, e.getMessage());
+					Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+				}
+				
+				return true;
+			
+			case R.id.menu3:
+				Toast.makeText(this, "menu3", Toast.LENGTH_SHORT).show();
+				return true;
+		
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
